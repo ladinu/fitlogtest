b5484fcb2fee217a490019a98101c790d95f0791
Ladinu Chandrasinghe <chandral@ds.ad.adp.com>
2015-03-17 11:04:09 -0700

    Added generated files

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fitlogtestApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the fitlogtestApp
+ */
+angular.module('fitlogtestApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name fitlogtestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the fitlogtestApp
+ */
+angular.module('fitlogtestApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
