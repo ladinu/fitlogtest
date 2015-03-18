55d790570d263faf86909dbb6618f062d74a91ca
Ladinu Chandrasinghe <ladinu@gmail.com>
2015-03-18 12:38:38 -0700

    Finished UI

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,12 +11,28 @@ angular.module('fitlogtestApp')
   .controller('MainCtrl', function ($scope, $resource) {
 
     var User = $resource('/api/users/:usedId', {userId: '@id'});
+    var Routine = $resource('/api/users/:userId/routines', {userId: '@userId'});
 
     $scope.userName = '';
+    $scope.user = null;
+    $scope.routineName = '';
+    $scope.routines = [];
 
     $scope.addUser = function() {
-      var u = new User({'userName': 'foo'});
+      var u = new User({'userName': $scope.userName});
       u.$save();
+      $scope.user = u;
     };
+
+    $scope.addRoutine = function() {
+      var r = new Routine({'userId': $scope.user.id, 'routineName': $scope.routineName, 'workouts': []});
+      r.$save();
+    };
+
+    $scope.getRoutines = function() {
+      $scope.routines = Routine.query({'userId': $scope.user.id});
+    };
+
+
 
   });
